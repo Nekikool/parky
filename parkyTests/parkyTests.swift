da47775d08476a78be117b1bdd316aa60c32037b
Alexis Suard <asuard@ecocea.com>
2016-02-28 13:50:50 +0100

    Initial commit

---
+++ parkyTests/parkyTests.swift
@@ -0,0 +1,36 @@
+//
+//  parkyTests.swift
+//  parkyTests
+//
+//  Created by Alexis Suard on 28/02/2016.
+//  Copyright © 2016 Alexis Suard. All rights reserved.
+//
+
+import XCTest
+@testable import parky
+
+class parkyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
